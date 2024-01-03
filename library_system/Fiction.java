e7cb420b5da6295cd1be83c639cccc042ff80703
chathuranga-jayanath <chathuranga.19@cse.mrt.ac.lk>
2024-01-03 16:06:11 +0530

    update system

--- library_system/Fiction.java
+++ library_system/Fiction.java
@@ -3,5 +3,28 @@ package library_system;
 public class Fiction extends Book implements Borrowable {
     public void read() {
         System.out.println("This is a fiction book");
+        System.out.println("title: " + this.getName());
+    }
+
+    public void read(String pageContent) {
+        System.out.println("title: " + this.getName());
+        System.out.println(pageContent);
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println("this is a fiction book");
+    }
+
+    @Override
+    public void totalCopiesCount() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'totalCopiesCount'");
+    }
+
+    @Override
+    public void borrowedCopiesCount() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'borrowedCopiesCount'");
     }
 }
